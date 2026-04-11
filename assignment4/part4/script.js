4ef0d5fb100802ba907ca8afbdfe6d685d41c51e
IshaqAhadGit100 <ishaq.ahad@dcmail.ca>
2026-04-11 15:57:32 -0400

    commit 4
    added evil circle

--- assignment4/part4/script.js
+++ assignment4/part4/script.js
@@ -4,6 +4,9 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+const count = document.getElementById("count");
+let ballCount = 25;
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -60,6 +63,63 @@ class Ball extends Shape {
     }
   }
 }
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) this.x -= this.size;
+    if (this.x - this.size <= 0) this.x += this.size;
+    if (this.y + this.size >= height) this.y -= this.size;
+    if (this.y - this.size <= 0) this.y += this.size;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+            ballCount--;
+  count.textContent = ballCount;
+        }
+      }
+    }
+
+   }
+  }
+
 
   const balls = [];
   while (balls.length < 25) {
@@ -77,6 +137,11 @@ class Ball extends Shape {
   balls.push(ball);
 }
 
+const evil = new EvilCircle(
+random(0 + 10, width - 10),
+random(0 + 10, height - 10)
+);
+
 function loop() {
   ctx.fillStyle = "rgb(0 0 0 / 25%)";
   ctx.fillRect(0, 0, width, height);
@@ -88,6 +153,11 @@ function loop() {
     ball.collisionDetect();
   }
 }
+
+evil.draw();
+  evil.checkBounds();
+  evil.collisionDetect();
+
   requestAnimationFrame(loop);
 
 }
