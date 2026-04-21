9f8a54120ac087f7178a8de22dda0bfabf0fd3bd
IshaqAhadGit100 <ishaq.ahad@dcmail.ca>
2026-04-21 17:40:54 -0400

    commit 4
    Replaced div with semantic button element, Added aria expanded for accessibility, Improved JavaScript event handling for toggle functionality and html semantic

--- assignment5/main.js
+++ assignment5/main.js
@@ -4,16 +4,19 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
-showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
+showHideBtn.addEventListener('click', () => {
+  const isHidden = commentWrapper.style.display === 'none';
+
+  if (isHidden) {
     showHideBtn.textContent = 'Hide comments';
     commentWrapper.style.display = 'block';
+    showHideBtn.setAttribute('aria-expanded', 'true');
   } else {
     showHideBtn.textContent = 'Show comments';
     commentWrapper.style.display = 'none';
+    showHideBtn.setAttribute('aria-expanded', 'false');
   }
-};
+});
 
 // functionality for adding a new comment via the comments form
 
